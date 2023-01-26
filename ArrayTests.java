0668600382d649eac24c2c88b18e409ad4ec0491
wsx2021 <80902225+wsx2021@users.noreply.github.com>
2023-01-25 20:06:26 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,23 +4,23 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = {1};
+    int[] input1 = {1,3,6};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{1}, input1);
+    assertArrayEquals(new int[]{6,3,1}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = {2};
-    assertArrayEquals(new int[]{2}, ArrayExamples.reversed(input1));
+    int[] input1 = {1,2,4,5};
+    assertArrayEquals(new int[]{5,4,2,1}, ArrayExamples.reversed(input1));
   }
 
   @Test
   public void testAvg() {
-    double[] input1 = {3,3};
+    double[] input1 = {3,5,3};
     System.out.println("test3 "+ArrayExamples.averageWithoutLowest(input1)); 
-    assertEquals(3,ArrayExamples.averageWithoutLowest(input1),0);
+    assertEquals(4,ArrayExamples.averageWithoutLowest(input1),0);
 
   }
 }
